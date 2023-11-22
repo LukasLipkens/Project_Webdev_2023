3f5d14e9e53a77429ddd4b9a87d22118cb1fa89f
LukasLipkens <lukas.lipkens1@gmail.com>
2023-11-22 20:57:39 +0100

    tamplatje toegevoegd

--- testingAreaLukas/test_navigatie/home.js
+++ testingAreaLukas/test_navigatie/home.js
@@ -28,29 +28,29 @@ class app extends HTMLElement
         this.shadow = this.attachShadow({mode: "open"}) // zorgt ervoor dart het component een afgeschermde stijl kan hebben
         this.shadow.append(template.content.cloneNode(true))
         this.container = this.shadow.querySelector("#homeContainer");
-        this.matchesArray = ["field"];
+        this.matchesArray = ["field1", "field2"];
     }
     
-    static get observedAttributes(){
-        return["hasupdate"];
-    }
+    // static get observedAttributes(){
+    //     return["hasupdate"];
+    // }
     
-    attributeChangedCallback(name, oldValue, newValue){
-        if(name == "hasupdate"){
-            console.log("attribute changed: " + name + ", " + newValue)
-            if(newValue == "true" && oldValue == "false"){
+    // attributeChangedCallback(name, oldValue, newValue){
+    //     if(name == "hasupdate"){
+    //         console.log("attribute changed: " + name + ", " + newValue)
+    //         if(newValue == "true" && oldValue == "false"){
 
                 
 
-                this.matchesArray.push(this.matchesArray.length+1); //ipv een push moet er hier eigenlijk een request naar de database gestuurd worden om de spellen op te vragen die op dat moment gespeeld worden
+    //             this.matchesArray.push(this.matchesArray.length+1); //ipv een push moet er hier eigenlijk een request naar de database gestuurd worden om de spellen op te vragen die op dat moment gespeeld worden
 
-                console.log(this.matchesArray);
+    //             console.log(this.matchesArray);
 
-                this.updateContainer();
-                this.setAttribute("hasupdate", "false");
-            }
-        }
-    }
+    //             this.updateContainer();
+    //             this.setAttribute("hasupdate", "false");
+    //         }
+    //     }
+    // }
 
     connectedCallback(){//hier het aantal matches tonen
         for(let match = 0; match < this.matchesArray.length; match++){
@@ -63,19 +63,19 @@ class app extends HTMLElement
         }
     }
 
-    updateContainer(){
-        this.matchesArray.forEach(element => {
-            let board = this.shadow.querySelector(`#scoreboard_${element}`);
-            if(board != null){}
-            else{
-                let scoreBord = document.createElement(`score-comp`);
-                scoreBord.setAttribute("id", `scoreboard_${element}`);
+    // updateContainer(){
+    //     this.matchesArray.forEach(element => {
+    //         let board = this.shadow.querySelector(`#scoreboard_${element}`);
+    //         if(board != null){}
+    //         else{
+    //             let scoreBord = document.createElement(`score-comp`);
+    //             scoreBord.setAttribute("id", `scoreboard_${element}`);
 
-                this.container.append(scoreBord);
-                console.log("Added: " + scoreBord.getAttribute("id"));
-            }
-        });
-    }
+    //             this.container.append(scoreBord);
+    //             console.log("Added: " + scoreBord.getAttribute("id"));
+    //         }
+    //     });
+    // }
 }
 
 customElements.define('home-comp', app)
