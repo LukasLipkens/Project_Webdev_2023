62409cce9469fb8a12ea1001920396051f259228
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2023-11-26 18:42:48 +0100

    debug

--- Website/Debug/index.php
+++ Website/Debug/index.php
@@ -15,3 +15,24 @@ if ($conn->connect_error) {
 } else {
     echo ("succes");
 }
+$name = $_GET["name"];
+$password = $_GET["password"];
+$nameErr = $passErr = "";
+
+
+$t_sql = 'SELECT * FROM tblspelers WHERE gebruikersnaam= "' . $name . '" AND password ="' . $password . '";';
+$t_result = mysqli_query($conn, $t_sql);
+if ($t_result) {
+    $t_arr = mysqli_fetch_all($t_result, MYSQLI_ASSOC);
+    if (empty($t_arr)) {
+        echo "Error: user not found or password not correct";
+    } else {
+        session_start();
+        $_SESSION['user'] = $t_arr[0];
+        // var_dump($t_arr);
+        // var_dump($_SESSION);
+        print_r($t_arr);
+    }
+} else {
+    echo "Error:" . mysqli_error($conn);
+}
