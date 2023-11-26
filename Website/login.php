280f5fc522a89ef76e233afb779403063be919fa
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2023-11-26 18:43:42 +0100

    debug22222222

--- Website/login.php
+++ Website/login.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'tennisscore.duckdns.org');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'super');
 define('DB_PASS', 'Project_Webdev');
 define('DB_NAME', 'project_tennis');
