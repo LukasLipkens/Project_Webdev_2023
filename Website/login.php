06a1a1dd3d4e667f9f69ed2df8a8a137904769bc
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2023-11-28 00:53:09 +0100

    update_login

--- Website/login.php
+++ Website/login.php
@@ -18,27 +18,23 @@ session_start();
 
 $modus = $_GET["modus"];
 
-if ($modus == 0 && isset($_SESSION["user"])) {
-    print_r(true);
-} else {
-    $email = $_GET["email"];
-    $password = $_GET["password"];
-    $nameErr = $passErr = "";
+$email = $_GET["email"];
+$password = $_GET["password"];
+$nameErr = $passErr = "";
 
 
-    $t_sql = 'SELECT * FROM tblspelers WHERE email= "' . $email . '" AND password ="' . $password . '";';
-    $t_result = mysqli_query($conn, $t_sql);
-    if ($t_result) {
-        $t_arr = mysqli_fetch_all($t_result, MYSQLI_ASSOC);
-        if (empty($t_arr)) {
-            echo "Error: user not found or password not correct";
-        } else {
-            $_SESSION['user'] = $t_arr[0];
-            // var_dump($t_arr);
-            var_dump($_SESSION["user"]);
-            // echo "succes";
-        }
+$t_sql = 'SELECT * FROM tblspelers WHERE email= "' . $email . '" AND password ="' . $password . '";';
+$t_result = mysqli_query($conn, $t_sql);
+if ($t_result) {
+    $t_arr = mysqli_fetch_all($t_result, MYSQLI_ASSOC);
+    if (empty($t_arr)) {
+        echo "Error: user not found or password not correct";
     } else {
-        echo "Error:" . mysqli_error($conn);
+        $_SESSION['user'] = $t_arr[0];
+        // var_dump($t_arr);
+        var_dump($_SESSION["user"]);
+        // echo "succes";
     }
+} else {
+    echo "Error:" . mysqli_error($conn);
 }
