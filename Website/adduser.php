06c747b1e698a58072f2eae98249b2e743eaca5f
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2023-12-01 11:07:06 +0100

    lpl

--- Website/adduser.php
+++ Website/adduser.php
@@ -8,13 +8,13 @@ define('DB_NAME', 'project_tennis');
 
 // Create connection
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
 
-if ($conn->connect_error) {
-    die("Connection failed $conn->connect_error");
-}
+// if ($conn->connect_error) {
+//     die("Connection failed $conn->connect_error");
+// }
 
 $name = $_GET["name"];
 $email = $_GET["email"];
