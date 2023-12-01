e3bfc37aa8eee2ca5cabc5c94c985c9cd02375ac
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2023-12-01 11:25:49 +0100

    lpl

--- Website/voegusertoe.php
+++ Website/voegusertoe.php
@@ -6,7 +6,21 @@ $email = $_GET["email"];
 $password = $_GET["password"];
 
 
-echo "test";
+$t_sql = 'SELECT * FROM tblspelers WHERE email="niels@gmail.com" AND password="elo";';
+$t_result = mysqli_query($conn, $t_sql);
+if ($t_result) {
+    $t_arr = mysqli_fetch_all($t_result, MYSQLI_ASSOC);
+    if (empty($t_arr)) {
+        echo "Error: user not found or password not correct";
+    } else {
+        $_SESSION['user'] = $t_arr[0];
+        // var_dump($t_arr);
+        var_dump($_SESSION["user"]);
+        // echo "succes";
+    }
+} else {
+    echo "Error:" . mysqli_error($conn);
+}
 // if (CheckExistingUser($email, $conn)) {
 //     $t_sql = 'INSERT INTO tblspelers (gebruikersnaam, password, email) VALUES("' . $name . '", "' . $password . '", "' . $email . '");';
 
