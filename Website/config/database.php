9e410d8dd1124251c08322b414723d91e0bb5b25
LukasLipkens <lukas.lipkens1@gmail.com>
2023-12-19 23:58:04 +0100

    password voor server

--- Website/config/database.php
+++ Website/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'Brad');
-define('DB_PASS', 'eloict');
+define('DB_USER', 'super');
+define('DB_PASS', 'Project_Webdev');
 define('DB_NAME', 'project_tennis');
 
 // Create connection
