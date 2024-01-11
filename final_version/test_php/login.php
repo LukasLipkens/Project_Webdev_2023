5c28e28df36356f368ba7b7afa9e238554317818
Nielskepielske <94488158+Nielskepielske@users.noreply.github.com>
2024-01-11 15:43:11 +0100

    include

--- final_version/test_php/login.php
+++ final_version/test_php/login.php
@@ -1,19 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'Brad');
-define('DB_PASS', 'eloict');
-define('DB_NAME', 'project_tennis');
-
-// Create connection
-
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// Check connection
-
-if ($conn->connect_error) {
-    die("Connection failed $conn->connect_error");
-    echo ("Connectie mislukt");
-}
+include_once "../config/database.php";
 session_start();
 
 //$modus = $_GET["modus"];
